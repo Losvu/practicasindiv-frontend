7d6b56c8180ac5e4052fff1cf58fc46baef49073
Losvu <teodorocairo096@gmail.com>
2025-04-01 22:00:32 -0600

    nose>2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./views/Login";
 import Inicio from "./views/Inicio";
+import Clientes from "./views/clientes";
 import './App.css';
+import Encabezado from "./components/encabezado/Encabezado";
+import Producto from "./views/productos";
+import Categorias from "./views/Categorias";
+import Ventas from "./views/Ventas";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-
-        <Route path="/" element={<Login />} />
-        <Route path="/inicio" element={<Inicio />} />
-
-      </Routes>
+      <Router>
+        //Contenedor principal con margen superior
+      <main className="margen-superior-main">
+      <Encabezado/>    
+        <Routes>
+         //Definicion de rutas
+          <Route path="/" element={<Login />} />
+          <Route path="/inicio" element={<Inicio />} />
+          <Route path="/clientes" element={<Clientes/>} />
+          <Route path="/productos" element={<Producto/>} />
+          <Route path="/categorias" element={<Categorias/>} />
+          <Route path="/ventas" element={<Ventas/>} />
+        </Routes>
+      </main>
     </Router>
   );
 };
